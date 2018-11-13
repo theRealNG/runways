e653e573106d0d6f6e0228f644468beea3bc0348
NG Sai Prasanth <ngsaip7@gmail.com>
2018-11-14 02:57:20 +0530

    ActiveRecord: added migration rake task (#1)

--- lib/runways/generators/app_structure_generator.rb
+++ lib/runways/generators/app_structure_generator.rb
@@ -15,11 +15,16 @@ class AppStructure < Thor::Group
     models_directory = app_directory + "/models"
     controller_directory = app_directory + "/controllers"
     db_directory = root_directory + "/db"
+    migration_directory = db_directory + "/migrate"
     lib_directory = root_directory + "/lib"
+    tasks_directory = lib_directory + "/tasks"
+    config_directory = root_directory + "/config"
 
     create_following_dirs([
-      root_directory, app_directory, db_directory,
-      models_directory, controller_directory, lib_directory,
+      root_directory, app_directory, config_directory,
+      lib_directory, tasks_directory,
+      models_directory, controller_directory,
+      db_directory, migration_directory,
     ])
   end
 
@@ -30,6 +35,14 @@ class AppStructure < Thor::Group
 
   def generate_gemfile
     template("templates/gemfile.tt", "#{name}/Gemfile")
+  end
+
+  def generate_db_rake_file
+    template("templates/db_rake.tt", "#{name}/lib/tasks/db.rake")
+  end
+
+  def generate_rake_file
+    template("templates/rakefile.tt", "#{name}/Rakefile")
   end
 
   def generate_application_record
@@ -58,6 +71,10 @@ class AppStructure < Thor::Group
       "templates/server.tt",
       "#{name}/#{name.underscore}_server.rb"
     )
+  end
+
+  def generate_db_config_file
+    template("templates/db_config.tt", "#{name}/config/db_config.rb")
   end
 
   private
