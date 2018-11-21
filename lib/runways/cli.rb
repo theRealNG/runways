a6e35ccafff15c3ab248fc8d8db8c39e1b361268
NG <ngsaip7@gmail.com>
2018-11-21 22:53:40 +0530

    CLI: Refactor CLI to provide 'new' for creating the app

---
+++ lib/runways/cli.rb
@@ -0,0 +1,10 @@
+module Runways
+  class CLI < Thor
+    desc "new name", "provide name for the framework"
+    def new(app_name)
+      puts "building framework #{app_name}"
+
+      AppStructure.start([app_name])
+    end
+  end
+end
